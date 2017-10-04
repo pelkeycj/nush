8fdac700c87653718c8ca75ee9cd7fb45a1552ad
Connor Pelkey <Pelkey.c@husky.neu.edu>
2017-10-04 16:31:55 -0400

    pass basic echo and loop tests

--- nush.c
+++ nush.c
@@ -15,62 +15,86 @@ execute(char* cmd)
 
   if ((cpid = fork())) {
     // parent process
-    printf("Parent pid: %d\n", getpid());
-    printf("Parent knows child pid: %d\n", cpid);
+    //printf("Parent pid: %d\n", getpid());
+    //printf("Parent knows child pid: %d\n", cpid);
 
     // Child may still be running until we wait.
 
     int status;
     waitpid(cpid, &status, 0);
 
-    printf("== execu  ted program complete ==\n");
+    //printf("== executed program complete ==\n");
 
-    printf("child returned with wait code %d\n", status);
+    //printf("child returned with wait code %d\n", status);
     if (WIFEXITED(status)) {
-      printf("child exited with exit code (or main returned) %d\n", WEXITSTATUS(status));
+      //printf("child exited with exit code (or main returned) %d\n", WEXITSTATUS(status));
     }
   }
   else {
     // child process
-    printf("Child pid: %d\n", getpid());
-    printf("Child knows parent pid: %d\n", getppid());
+    //printf("Child pid: %d\n", getpid());
+    //printf("Child knows parent pid: %d\n", getppid());
 
+/*
     for (int ii = 0; ii < strlen(cmd); ++ii) {
       if (cmd[ii] == ' ') {
         cmd[ii] = 0;
         break;
       }
     }
+    */
+
+
 
     // The argv array for the child.
     // Terminated by a null pointer.
-    char* args[] = {cmd, "one", 0};
+    //char* args[] = {cmd, "one", 0};
 
     printf("== executed program's output: ==\n");
 
-    //TODO tokenize cmd, create array char* args of arguments
+    cvector* cv = new_cvector();
+    tokenize(cv, cmd, strlen(cmd));
+    cmd = cv->items[0];
+
+    // create arguments array and init with command
+    char* args[cv->size  + 2];
+    args[0] = cmd;
+
+    for (int i = 1; i < cv->size; i++) {
+      args[i + 1] = cv->items[i];
+    }
+    args[cv->size + 1] = 0; // null terminate
 
     execvp(cmd, args);
-    printf("Can't get here, exec only returns on error.");
+
+    // free cvector
+    free_cvector(cv);
   }
 }
 
 int
 main(int argc, char* argv[])
 {
-  char cmd[256];
 
-  if (argc == 1) {
-    printf("nush$ ");
-    fflush(stdout);
-    fgets(cmd, 256, stdin);
+  while (1) {
+    char cmd[256];
+
+    if (argc == 1) {
+      printf("nush$ ");
+      fflush(stdout);
+      char* x = fgets(cmd, 256, stdin);
+      if (!x) {
+        return 0;
+      }
+    }
+    else {
+      memcpy(cmd, "echo", 5);
+    }
+
+    execute(cmd);
+
+
   }
-  else {
-    memcpy(cmd, "echo", 5);
-  }
-
-  execute(cmd);
-
   return 0;
 }
 
--- tokens.h
+++ tokens.h
@@ -1,8 +1,9 @@
 #ifndef TOKENS_H
 #define TOKENS_H
 
+#include "cvector.h"
 
-// split input into tokens in a cvector 
+// split input into tokens in a cvector
 void tokenize(cvector* cv, char* input, int maxLength);
 
 #endif
