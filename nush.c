710207f7378159c2bf19e8b8cf8b723123ef7fb6
Connor Pelkey <Pelkey.c@husky.neu.edu>
2017-10-04 17:34:27 -0400

    separate functions for user/script loops

--- nush.c
+++ nush.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 #include "cvector.h"
 #include "tokens.h"
@@ -19,47 +20,17 @@ int exitcmd(cvector* cv) {
   return 0;
 }
 
+// execute a command
 void execute(cvector* cv)
 {
   int cpid;
 
   if ((cpid = fork())) {
-    // parent process
-    //printf("Parent pid: %d\n", getpid());
-    //printf("Parent knows child pid: %d\n", cpid);
-
     // Child may still be running until we wait.
-
     int status;
     waitpid(cpid, &status, 0);
-
-    //printf("== executed program complete ==\n");
-
-    //printf("child returned with wait code %d\n", status);
-    if (WIFEXITED(status)) {
-      //printf("child exited with exit code (or main returned) %d\n", WEXITSTATUS(status));
-    }
   }
   else {
-    // child process
-    //printf("Child pid: %d\n", getpid());
-    //printf("Child knows parent pid: %d\n", getppid());
-
-/*
-    for (int ii = 0; ii < strlen(cmd); ++ii) {
-      if (cmd[ii] == ' ') {
-        cmd[ii] = 0;
-        break;
-      }
-    }
-    */
-
-
-
-    // The argv array for the child.
-    // Terminated by a null pointer.
-    //char* args[] = {cmd, "one", 0};
-
     printf("== executed program's output: ==\n");
 
     char* cmd = cv->items[0];
@@ -76,38 +47,74 @@ void execute(cvector* cv)
   }
 }
 
-int main(int argc, char* argv[])
-{
+// loop function to read and process user input
+void userLoop() {
+  while (1) {
+    char cmd[256];
+    cvector* cv = new_cvector();
+
+    // get input
+    printf("nush$ ");
+    fflush(stdout);
+    char* x = fgets(cmd, 256, stdin);
+    if (!x) {
+      free_cvector(cv);
+      return;
+    }
+
+    // tokenize input
+    tokenize(cv, cmd, strlen(cmd));
+    if (exitcmd(cv)) {
+      free_cvector(cv);
+      return;
+    }
+
+    // execute
+    execute(cv);
+    free_cvector(cv);
+  }
+
+}
+
+// loop function to read and process script input
+void scriptLoop(char* argv[]) {
+  // open file
+  int file = open(argv[1], O_WRONLY);
+
+//TODO loop on input?
 
   while (1) {
     char cmd[256];
     cvector* cv = new_cvector();
 
-    if (argc == 1) {
-      printf("nush$ ");
-      fflush(stdout);
-      char* x = fgets(cmd, 256, stdin);
-      if (!x) {
-        break;
-      }
-      // tokenize input
-      tokenize(cv, cmd, strlen(cmd));
-      if (exitcmd(cv)) {
-        break;
-      }
+    //TODO read file
+    /*
+    if (!x) {
+      free_cvector(cv);
+      return;
     }
-    else {
-      // read file
-      int file = open(argv[1], O_CREAT | O_TRUNC | O_WRONLY);
+    */
+    // tokenize input
+    tokenize(cv, cmd, strlen(cmd));
+    if (exitcmd(cv)) {
+      free_cvector(cv);
+      return;
     }
 
-
+    // execute
     execute(cv);
     free_cvector(cv);
-
   }
-  return 0;
 }
 
+int main(int argc, char* argv[])
+{
+  if (argc == 1) {
+      userLoop();
+    }
+  else {
+    scriptLoop(argv);
+  }
 
-//TODO: cd, exit
+  return 0;
+}
